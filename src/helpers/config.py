cccf8be162d73323bba441ec66d458eb596faf0d
Mohammed Sameh <95502692+MohamedSameh410@users.noreply.github.com>
2025-03-15 04:40:59 +0200

    setup MongoDB

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,9 +4,13 @@ class settings(BaseSettings):
 
     APP_NAME: str
     OPENAI_API_KEY: str
+
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_CHUNK_SIZE: int
+
+    MONGO_URI: str
+    MONGO_DB: str
 
     class Config:
         env_file = ".env"
--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from routes import base, data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings, settings
 
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client(app_settings: settings = Depends(get_settings)):
+    
+    app.mongo_conn = AsyncIOMotorClient(app_settings.MONGO_URI)
+    app.db = app.mongo_conn[app_settings.MONGO_DB]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client(app: FastAPI):
+    app.mongo_conn.close()
+    
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
