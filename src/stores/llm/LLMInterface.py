b56e1d4dbe2778d42b76efd3a0915e99f5a12a59
Mohammed Sameh <95502692+MohamedSameh410@users.noreply.github.com>
2025-04-07 06:29:20 +0200

    update the LLM interface

--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -10,21 +10,21 @@ class LLMInterface(ABC):
         pass
 
     @abstractmethod
-    def set_embedding_model(self, model_id: str):
+    def set_embedding_model(self, model_id: str, embedding_size: int):
         """
         Set the embedding model to be used.
         """
         pass
 
     @abstractmethod
-    def generate_text(self, prompt: str, max_output_tokens: int, temperature: float = None):
+    def generate_text(self, prompt: str, chat_history: list = [], max_output_tokens: int = None, temperature: float = None):
         """
         Generate text based on the provided prompt.
         """
         pass
 
     @abstractmethod
-    def embed_text(self, text: str, document_type: str):
+    def embed_text(self, text: str, document_type: str = None):
         """
         Embed the provided text.
         param document_type: The type of document to embed(user query || chunk).
